baad86066040fcb7630a2956b9dec042585a1a7a
Peter Chen <manchesterutd7@gmail.com>
2025-02-03 23:39:50 +0400

    chore: add Google Search Console verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useMemo, useState } from 'react'
 
+import type { Metadata } from 'next'
+
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useAtom } from 'jotai'
 import { minBy } from 'lodash'
@@ -32,6 +34,12 @@ import { Input } from '@/components/ui/input'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useMetadata, useTeamDetails, useTeamHistory } from '@/hooks/api'
 import { teamIdAtom } from '@/store'
+
+export const meatadat: Metadata = {
+  verification: {
+    google: 'iTDbvkxf0Uls4YGnfxEKt_Sq6Vjti6_GHCTFg0Iv3xg',
+  },
+}
 
 const formSchema = z.object({
   teamId: z.coerce.number().min(1),
